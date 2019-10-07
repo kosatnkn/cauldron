39cd946a508740aceef8e02bce566de510bc55fa
Kosala Tennakoon <kosala.me@gmail.com>
2019-10-07 12:09:44 +0530

    added process

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	fmt.Println("Hello")
+	// command
+	// $ cauldron
+
+	// get input params
+	// - New project name (have an input tag --name / -n)
+	// - Catalyst version to be used (have an input tag --tag=v1.0.0 / -t (default: latest))
+	// - openapi.yaml file (have an input tag --spec)
+	//  - validate naming conventions (have an input tag --strict=false / -s (default: true) to ignore this validaton)
+	//  - create base structures for unpackers using request body
+	//  - create transformers using response body
+
+	// interactive mode
+	// when tags are not provided use interactive mode
+
+	// process
+	// - clone
+	// - checkout tag
+	// - remove .git
+	// - generate structures
+	// - create splash using name
+	// - init as new git repo
 }
